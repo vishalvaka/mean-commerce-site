0f518591cb84ca8124705d8691286def7135a156
vishalvaka <vishalvaka@gmail.com>
2024-01-17 11:40:06 -0600

    added a simple display page and a login path

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express  =  require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express()
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    fs.readFile('frontend/index.html', 'utf8', (err, html) => {
+        if (err) {
+            res.status(500).send('Sorry, something went wrong');
+            return;
+        }
+        res.send(html);
+    });
 });
 
 const PORT = process.env.PORT || 3000;
@@ -12,8 +20,8 @@ app.listen(PORT, () => {
     console.log('Server is running on port ${PORT}');
 });
 
-const mongoose = requre('mongoose');
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/ecommerce-app', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB...', err));
