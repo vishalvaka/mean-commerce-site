237b7e881727090cca236f33d595f4d928897658
vishalvaka <vishalvaka@gmail.com>
2024-01-18 10:45:59 -0600

    made changes for login page and added a login.html file

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,16 @@ app.listen(PORT, () => {
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://192.168.50.198:27017/ecommerce-app', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB...', err));
+
+app.get('/login', (req, res) => {
+    fs.readFile('frontend/login.html', 'utf8', (err, html) => {
+        if (err) {
+            res.status(500).send('Sorry, something went wrong');
+            return;
+        }
+        res.send(html);
+    });
+});
